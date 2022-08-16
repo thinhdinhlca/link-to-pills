2ac7fe16433debd406847ba7a624a72bee094929
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-08-16 19:27:45 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 // https://www.chartjs.org/
 
 
-window.function = function (coordinates,center) {
+window.function = function (coordinates,center,zoom) {
 
   // data
  coordinates = coordinates.value ?? "";
  center = center.value ?? "";
+ zoom = zoom.value ?? "";
  
 
   let ht = `<!DOCTYPE html>
@@ -28,8 +29,8 @@ body { margin: 0; padding: 0; }
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
-center: [-122.486052, 37.830348],
-zoom: 15
+center: [${center}],
+zoom: ${zoom}
 });
  
 map.on('load', () => {
@@ -40,29 +41,7 @@ map.addSource('route', {
 'properties': {},
 'geometry': {
 'type': 'LineString',
-'coordinates': [
-[-122.483696, 37.833818],
-[-122.483482, 37.833174],
-[-122.483396, 37.8327],
-[-122.483568, 37.832056],
-[-122.48404, 37.831141],
-[-122.48404, 37.830497],
-[-122.483482, 37.82992],
-[-122.483568, 37.829548],
-[-122.48507, 37.829446],
-[-122.4861, 37.828802],
-[-122.486958, 37.82931],
-[-122.487001, 37.830802],
-[-122.487516, 37.831683],
-[-122.488031, 37.832158],
-[-122.488889, 37.832971],
-[-122.489876, 37.832632],
-[-122.490434, 37.832937],
-[-122.49125, 37.832429],
-[-122.491636, 37.832564],
-[-122.492237, 37.833378],
-[-122.493782, 37.833683]
-]
+'coordinates': [${coordinates}]
 }
 }
 });
