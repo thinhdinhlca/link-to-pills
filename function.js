5f6df18b18db477296c097639ca067c2355de9ff
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-08-22 10:29:43 +0700

    Update function.js

--- function.js
+++ function.js
@@ -56,65 +56,6 @@ const nav = new mapboxgl.NavigationControl({
 visualizePitch: true
 });
 map.addControl(nav, 'bottom-right');
- 
-map.on('load', () => {
-map.addSource('places', {
-'type': 'geojson',
-'data': {
-'type': 'FeatureCollection',
-'features': [
-{
-'type': 'Feature',
-'properties': {
-'description':
-'<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>'
-}
-]
-}
-});
-
-map.addLayer({
-'id': 'places',
-'type': 'circle',
-'source': 'places',
-'paint': {
-'circle-color': '#4264fb',
-'circle-radius': 6,
-'circle-stroke-width': 2,
-'circle-stroke-color': '#ffffff'
-}
-});
- 
-// Create a popup, but don't add it to the map yet.
-const popup = new mapboxgl.Popup({
-closeButton: false,
-closeOnClick: false
-});
- 
-map.on('mouseenter', 'places', (e) => {
-// Change the cursor style as a UI indicator.
-map.getCanvas().style.cursor = 'pointer';
- 
-// Copy coordinates array.
-const coordinates = e.features[0].geometry.coordinates.slice();
-const description = e.features[0].properties.description;
- 
-// Ensure that if the map is zoomed out such that multiple
-// copies of the feature are visible, the popup appears
-// over the copy being pointed to.
-while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-}
- 
-// Populate the popup and set its coordinates
-// based on the feature found.
-popup.setLngLat(coordinates).setHTML(description).addTo(map);
-});
- 
-map.on('mouseleave', 'places', () => {
-map.getCanvas().style.cursor = '';
-popup.remove();
-});
 
 map.on('load', () => {
 map.addSource('route', {
