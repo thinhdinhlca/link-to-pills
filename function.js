9d67960514eb84627d4b6e7896af1946edf357c2
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-09-02 11:15:40 +0700

    Update function.js

--- function.js
+++ function.js
@@ -65,7 +65,7 @@ const geojson = {
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
+    style: 'mapbox://styles/dylandickman/cl0txkrs4000s16mobxi0nqin',
     center: [${center}],
     zoom: 3
 });
