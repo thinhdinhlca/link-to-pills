4ca0b3984d24dc54e17e56b9bb519f941bbbf426
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2023-12-16 17:42:53 +0700

    Update function.js

--- function.js
+++ function.js
@@ -11,60 +11,23 @@ window.function = function (centerString, radius) {
   let htmlContent = `<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Map with Circle</title>
-<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js"></script>
-<script src='https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js'></script>
+<meta charset=utf-8 />
+<title>A simple map</title>
+<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+<script src='https://api.mapbox.com/mapbox.js/v3.3.1/mapbox.js'></script>
+<link href='https://api.mapbox.com/mapbox.js/v3.3.1/mapbox.css' rel='stylesheet' />
 <style>
-body {
-margin: 0;
-padding: 0;
-}
-
-#map {
-position: absolute;
-top: 0;
-bottom: 0;
-width: 100%;
-}
+  body { margin:0; padding:0; }
+  #map { position:absolute; top:0; bottom:0; width:100%; }
 </style>
 </head>
 <body>
-<div id="map"></div>
+<div id='map'></div>
 <script>
-mapboxgl.accessToken = '${accessToken}';
-const center = [${centerString}];
-const radius = ${radius};
-const options = {steps: 50, units: 'kilometers'};
-const map = new mapboxgl.Map({
-    container: 'map',
-    center: [${centerString}],
-    zoom: 2
-});
-
-map.on('load', function () {
-    // Use Turf to create a circle feature
-    var circle = turf.circle(center, radius, options);
-
-    // Add the circle to the map
-    map.addSource('circle', {
-        type: 'geojson',
-        data: circle
-    });
-
-    map.addLayer({
-        id: 'circle',
-        type: 'fill',
-        source: 'circle',
-        paint: {
-            'fill-color': '#ADD8E6',
-            'fill-opacity': 0.5
-        }
-    });
-
-});
+L.mapbox.accessToken = '${accessToken}';
+var map = L.mapbox.map('map')
+    .setView([40, -74.50], 9)
+    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
 </script>
 </body>
 </html>`;
