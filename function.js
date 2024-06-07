1f0e25bdde9ffd92a9d4a0b39660877deb65278c
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2024-06-07 11:19:54 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,85 +1,19 @@
-window.function = function (centerString, radius, zoom) {
-  // Data
-  centerString = centerString.value ?? "";
-  radius = radius.value ?? 5;
-  zoom = zoom.value ?? 5;
+window.function = function(urls) {
+    // Data
+    urls = urls.value ?? "";
 
-  // Define your Mapbox access token
-  const accessToken = 'pk.eyJ1IjoidGhpbmhkaW5obGNhIiwiYSI6ImNsNnVvY2o2MTF3eTkzam9hcDRmeDJtbHQifQ.Nmmy2O9Zugnyy5gLvDTgHA';
+    // Decodes URL-encoded characters using decodeURIComponent
+    const decodeUrls = urls.map(url => decodeURIComponent(url));
 
-  // HTML content for the map
-  let htmlContent = `
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset=utf-8 />
-<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js"></script>
-<script src='https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js'></script>
-<script src='https://npmcdn.com/mapbox-gl-circle/dist/mapbox-gl-circle.min.js'></script>
-<style>
-body {
-  margin: 0;
-  padding: 0;
-}
-#map {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 100%;
-}
-</style>
-</head>
-<body>
-<div id="map"></div>
-<script>
-mapboxgl.accessToken = '${accessToken}';
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v8',
-    center: [${centerString}],
-    zoom: ${zoom},
-    interactive: true
-});
+    // Defines the inline CSS for the pill-style links
+    const linkStyle = 'padding: 8px 15px; background-color: #613DC7; color: white; text-decoration: none; border-radius: 20px; margin: 5px; display: inline-block; font-size: 14px;';
 
-// Add a circle to the map
-var myCircle = new MapboxCircle([${centerString}], ${radius}*1000, {
-        editable: false,
-        fillColor: '#29AB87'
-    }).addTo(map);
+    // Creates an HTML string with anchor tags for each URL, including the ↗ emoji
+    const linksHTML = decodeUrls.map(url => {
+        const fileName = url.split('/').pop();
+        return `<a href="${url}" target="_blank" style="${linkStyle}">↗ ${fileName}</a>`;
+    }).join('');
 
-// Add a marker to the map at the same coordinates as the circle's center
-var marker = new mapboxgl.Marker()
-    .setLngLat([${centerString}])
-    .addTo(map);
-    
-// Add click event to the map for reverse geocoding
-map.on('click', function(e) {
-    fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/' + e.lngLat.lng + ',' + e.lngLat.lat + '.json?types=poi&access_token=${accessToken}')
-        .then(response => response.json())
-        .then(data => {
-            var placeText = data.features.length > 0 ? data.features[0].text : 'Unknown Place';
-            var placeName = data.features.length > 0 ? data.features[0].place_name : 'No detailed address found';
-            var category = data.features.length > 0 && data.features[0].properties.category ? data.features[0].properties.category : 'No category found';
-
-            // Remove the place text from the place name to avoid duplication
-            var cleanedPlaceName = placeName.replace(placeText, '').replace(/^,|,$/g, '').trim();
-
-            new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML('<h3>' + placeText + '</h3><p>' + cleanedPlaceName + '</p><p style="font-style: italic; font-size: smaller;">' + "Categories: " + category + '</p>')
-                .addTo(map);
-        });
-});
-
-
-</script>
-</body>
-</html>`;
-
-  // Encode the HTML content to a data URI
-  let encodedHtmlContent = encodeURIComponent(htmlContent);
-  let dataUri = `data:text/html;charset=utf-8,${encodedHtmlContent}`;
-  return dataUri;
+    // Returns the full HTML string
+    return `<div class="links-component" style="margin-bottom: 10px;">${linksHTML}</div>`;
 }
